041eb07d22dfe2d331d2d1f6eaea056d5f885f56
Harold Acevedo <acevedojh@gmail.com>
2022-01-27 20:07:12 -0500

    consolida qrCodeGen

--- qrCodeGen.py
+++ qrCodeGen.py
@@ -1,10 +1,17 @@
+import csv
+from email import header
 import qrcode
 
 
-def qr_code(rowFile):
-    
-    img = qrcode.make(rowFile)
-    img.save('./codesQR/QR Code.png')
-    
+csvDataFile = input('\nPlease enter the csvFile: ')
+file = open(csvDataFile)
+csvreader = csv.reader(file)
+header = []
+header = next(csvreader)
+rows = []
 
+for row in csvreader:
+    row.append('8º1')
+    img = qrcode.make(row)
+    img.save(f'./codesQR/qr{row[0]}.png')
 
